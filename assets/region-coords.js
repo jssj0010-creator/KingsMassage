daaab07834b9468e9c0d418f38eecec9932b48a9
jssj0010-creator <jssj0010@gmail.com>
2025-09-17 15:31:03 +0900

    Add files via upload
    Signed-off-by: jssj0010-creator <jssj0010@gmail.com>

--- assets/region-coords.js
+++ assets/region-coords.js
@@ -1,33 +1,675 @@
-// region-coords.js
-// 좌표/줌 설정: 지역 페이지에서 #region-map 영역에 표시할 정적 지도용
-// 사용처: assets/region-map.js 가 window.REGION_COORDS 를 참조합니다.
-
+// assets/region-coords.js
 window.REGION_COORDS = {
-  // 서울특별시 중심(시청 인근)
-  seoul: {
-    center: { lat: 37.5665, lon: 126.9780 },
-    zoom: 12,
-    marker: { lat: 37.5665, lon: 126.9780 }
+  "seoul": {
+    "center": {
+      "lat": 37.5665,
+      "lon": 126.978
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5665,
+      "lon": 126.978
+    }
   },
-
-  // 경기도 중심(수원시청 인근, 도청 소재지 기준)
-  gyeonggi: {
-    center: { lat: 37.2636, lon: 127.0286 },
-    zoom: 10,
-    marker: { lat: 37.2636, lon: 127.0286 }
+  "gyeonggi": {
+    "center": {
+      "lat": 37.2636,
+      "lon": 127.0286
+    },
+    "zoom": 10,
+    "marker": {
+      "lat": 37.2636,
+      "lon": 127.0286
+    }
   },
-
-  // 인천광역시 중심(인천시청/구월동 인근)
-  incheon: {
-    center: { lat: 37.4563, lon: 126.7052 },
-    zoom: 12,
-    marker: { lat: 37.4563, lon: 126.7052 }
+  "incheon": {
+    "center": {
+      "lat": 37.4563,
+      "lon": 126.7052
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4563,
+      "lon": 126.7052
+    }
+  },
+  "seoul-강남": {
+    "center": {
+      "lat": 37.5172,
+      "lon": 127.0473
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5172,
+      "lon": 127.0473
+    }
+  },
+  "seoul-서초": {
+    "center": {
+      "lat": 37.4836,
+      "lon": 127.0327
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.4836,
+      "lon": 127.0327
+    }
+  },
+  "seoul-송파": {
+    "center": {
+      "lat": 37.5146,
+      "lon": 127.106
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5146,
+      "lon": 127.106
+    }
+  },
+  "seoul-마포": {
+    "center": {
+      "lat": 37.5663,
+      "lon": 126.9018
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5663,
+      "lon": 126.9018
+    }
+  },
+  "seoul-용산": {
+    "center": {
+      "lat": 37.5326,
+      "lon": 126.9905
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5326,
+      "lon": 126.9905
+    }
+  },
+  "seoul-종로": {
+    "center": {
+      "lat": 37.5733,
+      "lon": 126.9793
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5733,
+      "lon": 126.9793
+    }
+  },
+  "seoul-성동": {
+    "center": {
+      "lat": 37.5636,
+      "lon": 127.0369
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5636,
+      "lon": 127.0369
+    }
+  },
+  "seoul-광진": {
+    "center": {
+      "lat": 37.5386,
+      "lon": 127.0827
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5386,
+      "lon": 127.0827
+    }
+  },
+  "seoul-동대문": {
+    "center": {
+      "lat": 37.574,
+      "lon": 127.0396
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.574,
+      "lon": 127.0396
+    }
+  },
+  "seoul-성북": {
+    "center": {
+      "lat": 37.5894,
+      "lon": 127.0167
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5894,
+      "lon": 127.0167
+    }
+  },
+  "seoul-은평": {
+    "center": {
+      "lat": 37.6176,
+      "lon": 126.9227
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.6176,
+      "lon": 126.9227
+    }
+  },
+  "seoul-노원": {
+    "center": {
+      "lat": 37.6543,
+      "lon": 127.0565
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.6543,
+      "lon": 127.0565
+    }
+  },
+  "seoul-중랑": {
+    "center": {
+      "lat": 37.6061,
+      "lon": 127.0929
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.6061,
+      "lon": 127.0929
+    }
+  },
+  "seoul-중구": {
+    "center": {
+      "lat": 37.564,
+      "lon": 126.9976
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.564,
+      "lon": 126.9976
+    }
+  },
+  "seoul-강동": {
+    "center": {
+      "lat": 37.5301,
+      "lon": 127.1238
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5301,
+      "lon": 127.1238
+    }
+  },
+  "seoul-강서": {
+    "center": {
+      "lat": 37.5509,
+      "lon": 126.8495
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5509,
+      "lon": 126.8495
+    }
+  },
+  "seoul-양천": {
+    "center": {
+      "lat": 37.517,
+      "lon": 126.8665
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.517,
+      "lon": 126.8665
+    }
+  },
+  "seoul-구로": {
+    "center": {
+      "lat": 37.4955,
+      "lon": 126.8877
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.4955,
+      "lon": 126.8877
+    }
+  },
+  "seoul-금천": {
+    "center": {
+      "lat": 37.4569,
+      "lon": 126.8958
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.4569,
+      "lon": 126.8958
+    }
+  },
+  "seoul-동작": {
+    "center": {
+      "lat": 37.5124,
+      "lon": 126.9393
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5124,
+      "lon": 126.9393
+    }
+  },
+  "seoul-영등포": {
+    "center": {
+      "lat": 37.5264,
+      "lon": 126.8963
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.5264,
+      "lon": 126.8963
+    }
+  },
+  "seoul-관악": {
+    "center": {
+      "lat": 37.4784,
+      "lon": 126.9516
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.4784,
+      "lon": 126.9516
+    }
+  },
+  "seoul-강북": {
+    "center": {
+      "lat": 37.6398,
+      "lon": 127.0255
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.6398,
+      "lon": 127.0255
+    }
+  },
+  "seoul-도봉": {
+    "center": {
+      "lat": 37.6688,
+      "lon": 127.0471
+    },
+    "zoom": 13,
+    "marker": {
+      "lat": 37.6688,
+      "lon": 127.0471
+    }
+  },
+  "gyeonggi-성남": {
+    "center": {
+      "lat": 37.42,
+      "lon": 127.1265
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.42,
+      "lon": 127.1265
+    }
+  },
+  "gyeonggi-분당": {
+    "center": {
+      "lat": 37.385,
+      "lon": 127.1217
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.385,
+      "lon": 127.1217
+    }
+  },
+  "gyeonggi-수원": {
+    "center": {
+      "lat": 37.2636,
+      "lon": 127.0286
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.2636,
+      "lon": 127.0286
+    }
+  },
+  "gyeonggi-용인": {
+    "center": {
+      "lat": 37.2411,
+      "lon": 127.1775
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.2411,
+      "lon": 127.1775
+    }
+  },
+  "gyeonggi-고양": {
+    "center": {
+      "lat": 37.6584,
+      "lon": 126.832
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.6584,
+      "lon": 126.832
+    }
+  },
+  "gyeonggi-일산": {
+    "center": {
+      "lat": 37.676,
+      "lon": 126.77
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.676,
+      "lon": 126.77
+    }
+  },
+  "gyeonggi-부천": {
+    "center": {
+      "lat": 37.5036,
+      "lon": 126.766
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5036,
+      "lon": 126.766
+    }
+  },
+  "gyeonggi-김포": {
+    "center": {
+      "lat": 37.6154,
+      "lon": 126.7158
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.6154,
+      "lon": 126.7158
+    }
+  },
+  "gyeonggi-의정부": {
+    "center": {
+      "lat": 37.7381,
+      "lon": 127.045
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.7381,
+      "lon": 127.045
+    }
+  },
+  "gyeonggi-평택": {
+    "center": {
+      "lat": 36.9907,
+      "lon": 127.085
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 36.9907,
+      "lon": 127.085
+    }
+  },
+  "gyeonggi-시흥": {
+    "center": {
+      "lat": 37.38,
+      "lon": 126.805
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.38,
+      "lon": 126.805
+    }
+  },
+  "gyeonggi-안산": {
+    "center": {
+      "lat": 37.3219,
+      "lon": 126.8309
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.3219,
+      "lon": 126.8309
+    }
+  },
+  "gyeonggi-안양": {
+    "center": {
+      "lat": 37.3943,
+      "lon": 126.9568
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.3943,
+      "lon": 126.9568
+    }
+  },
+  "gyeonggi-과천": {
+    "center": {
+      "lat": 37.4265,
+      "lon": 126.989
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4265,
+      "lon": 126.989
+    }
+  },
+  "gyeonggi-광명": {
+    "center": {
+      "lat": 37.478,
+      "lon": 126.864
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.478,
+      "lon": 126.864
+    }
+  },
+  "gyeonggi-파주": {
+    "center": {
+      "lat": 37.7602,
+      "lon": 126.7798
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.7602,
+      "lon": 126.7798
+    }
+  },
+  "gyeonggi-하남": {
+    "center": {
+      "lat": 37.5393,
+      "lon": 127.2149
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5393,
+      "lon": 127.2149
+    }
+  },
+  "gyeonggi-구리": {
+    "center": {
+      "lat": 37.5943,
+      "lon": 127.1295
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5943,
+      "lon": 127.1295
+    }
+  },
+  "gyeonggi-남양주": {
+    "center": {
+      "lat": 37.6367,
+      "lon": 127.2165
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.6367,
+      "lon": 127.2165
+    }
+  },
+  "gyeonggi-군포": {
+    "center": {
+      "lat": 37.3617,
+      "lon": 126.935
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.3617,
+      "lon": 126.935
+    }
+  },
+  "gyeonggi-오산": {
+    "center": {
+      "lat": 37.1498,
+      "lon": 127.0772
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.1498,
+      "lon": 127.0772
+    }
+  },
+  "gyeonggi-광주": {
+    "center": {
+      "lat": 37.41,
+      "lon": 127.257
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.41,
+      "lon": 127.257
+    }
+  },
+  "incheon-인천": {
+    "center": {
+      "lat": 37.4563,
+      "lon": 126.7052
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4563,
+      "lon": 126.7052
+    }
+  },
+  "incheon-계양": {
+    "center": {
+      "lat": 37.5382,
+      "lon": 126.737
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5382,
+      "lon": 126.737
+    }
+  },
+  "incheon-부평": {
+    "center": {
+      "lat": 37.5082,
+      "lon": 126.7218
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.5082,
+      "lon": 126.7218
+    }
+  },
+  "incheon-미추홀": {
+    "center": {
+      "lat": 37.4636,
+      "lon": 126.6509
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4636,
+      "lon": 126.6509
+    }
+  },
+  "incheon-연수": {
+    "center": {
+      "lat": 37.4108,
+      "lon": 126.678
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4108,
+      "lon": 126.678
+    }
+  },
+  "incheon-송도": {
+    "center": {
+      "lat": 37.3894,
+      "lon": 126.6436
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.3894,
+      "lon": 126.6436
+    }
+  },
+  "incheon-남동": {
+    "center": {
+      "lat": 37.4485,
+      "lon": 126.7314
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4485,
+      "lon": 126.7314
+    }
+  },
+  "incheon-서구": {
+    "center": {
+      "lat": 37.545,
+      "lon": 126.675
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.545,
+      "lon": 126.675
+    }
+  },
+  "incheon-강화": {
+    "center": {
+      "lat": 37.746,
+      "lon": 126.487
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.746,
+      "lon": 126.487
+    }
+  },
+  "incheon-영종도": {
+    "center": {
+      "lat": 37.511,
+      "lon": 126.523
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.511,
+      "lon": 126.523
+    }
+  },
+  "incheon-중구": {
+    "center": {
+      "lat": 37.4738,
+      "lon": 126.6214
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4738,
+      "lon": 126.6214
+    }
+  },
+  "incheon-동구": {
+    "center": {
+      "lat": 37.4753,
+      "lon": 126.6437
+    },
+    "zoom": 12,
+    "marker": {
+      "lat": 37.4753,
+      "lon": 126.6437
+    }
   }
 };
-
-// 기본값 (정의되지 않은 경우 서울로 fallback)
-window.REGION_COORDS_DEFAULT = {
-  center: { lat: 37.5665, lon: 126.9780 },
-  zoom: 12,
-  marker: { lat: 37.5665, lon: 126.9780 }
-};
+window.REGION_COORDS_DEFAULT = {"center": {"lat": 37.5665, "lon": 126.978}, "zoom": 12, "marker": {"lat": 37.5665, "lon": 126.978}};
--- assets/region-map.js
+++ assets/region-map.js
@@ -1,63 +1,60 @@
 
-// assets/region-map.js (iframe version)
-// Replace static image with OSM embed iframe for higher reliability.
-// - Detect region from URL or #region-map[data-region]
-// - Uses window.REGION_COORDS (center {lat,lon} + zoom)
-// - Builds an OSM embed URL: https://www.openstreetmap.org/export/embed.html?bbox=...&marker=...
-// - No API key required.
-
+// assets/region-map.js (iframe, sub-region aware)
 (function(){
   function sel(q, root){ return (root||document).querySelector(q); }
-  function detectRegion(){
+  function detectKey(){
     var el = sel('#region-map');
-    if (el && el.dataset && el.dataset.region) return el.dataset.region.toLowerCase();
-    var m = (location.pathname || '').match(/\/regions\/(seoul|gyeonggi|incheon)(?:\.html|$)/i);
-    return m ? m[1].toLowerCase() : 'seoul';
+    if (el) {
+      var r = el.getAttribute('data-region');
+      var s = el.getAttribute('data-subregion');
+      if (r && s) return (r.toLowerCase() + "-" + s).trim();
+      if (r) return r.toLowerCase();
+    }
+    var path = decodeURIComponent(location.pathname || "");
+    // matches: /regions/seoul-강남.html OR /regions/seoul.html
+    var m = path.match(/\/regions\/(seoul|gyeonggi|incheon)(?:-([^.\/]+))?\.html/i);
+    if (m) {
+      if (m[2]) return (m[1].toLowerCase() + "-" + m[2]).trim();
+      return m[1].toLowerCase();
+    }
+    return "seoul";
   }
-  function coordsFor(region){
-    var def = { center:{lat:37.5665, lon:126.9780}, zoom:12, marker:{lat:37.5665, lon:126.9780} };
-    var data = (window.REGION_COORDS && window.REGION_COORDS[region]) || window.REGION_COORDS_DEFAULT || def;
-    // normalize
-    data.center = data.center || def.center;
-    data.marker = data.marker || data.center;
-    data.zoom = data.zoom || 12;
-    return data;
+  function getCfg(key){
+    var cfg = (window.REGION_COORDS && window.REGION_COORDS[key]);
+    if (!cfg){
+      // try parent key if sub not found
+      var p = key.split("-")[0];
+      cfg = (window.REGION_COORDS && window.REGION_COORDS[p]);
+    }
+    return cfg || window.REGION_COORDS_DEFAULT;
   }
-
-  // Approximate half-width/height degrees by zoom (simple heuristic good enough for city view)
   function zoomToDelta(zoom){
-    // Wider area for low zoom, tighter for high zoom
     if (zoom <= 9)  return { dlat: 0.6,  dlon: 0.8 };
     if (zoom === 10) return { dlat: 0.35, dlon: 0.5 };
     if (zoom === 11) return { dlat: 0.2,  dlon: 0.3 };
     if (zoom === 12) return { dlat: 0.1,  dlon: 0.15 };
-    if (zoom === 13) return { dlat: 0.05, dlon: 0.08 };
+    if (zoom >= 13) return { dlat: 0.05, dlon: 0.08 };
     return { dlat: 0.1, dlon: 0.15 };
   }
-
   function render(){
-    var host = sel('#region-map');
-    if (!host) return;
+    var host = sel('#region-map'); if (!host) return;
     if (host.getAttribute('data-map-rendered') === '1') return;
 
-    var region = detectRegion();
-    var cfg = coordsFor(region);
+    var key = detectKey();
+    var cfg = getCfg(key);
     var lat = +cfg.center.lat, lon = +cfg.center.lon;
-    var ml = cfg.marker || cfg.center;
-    var mlat = +ml.lat, mlon = +ml.lon;
-    var zoom = +cfg.zoom;
+    var mlat = +cfg.marker.lat, mlon = +cfg.marker.lon;
+    var zoom = +cfg.zoom || 12;
 
-    var delta = zoomToDelta(zoom);
-    var minlon = lon - delta.dlon, maxlon = lon + delta.dlon;
-    var minlat = lat - delta.dlat, maxlat = lat + delta.dlat;
+    var d = zoomToDelta(zoom);
+    var minlon = lon - d.dlon, maxlon = lon + d.dlon;
+    var minlat = lat - d.dlat, maxlat = lat + d.dlat;
 
-    // Build embed URL (no cookies/API)
     var url = "https://www.openstreetmap.org/export/embed.html"
       + "?bbox=" + encodeURIComponent([minlon, minlat, maxlon, maxlat].join(","))
       + "&layer=mapnik"
       + "&marker=" + encodeURIComponent(mlat + "," + mlon);
 
-    // Create iframe
     var iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.setAttribute('style', 'border:0;width:100%;height:260px');
@@ -65,19 +62,12 @@
     iframe.setAttribute('referrerpolicy', 'no-referrer');
     iframe.setAttribute('aria-label', '지역 위치 지도');
 
-    iframe.addEventListener('error', function(){
-      host.textContent = "지도를 불러오지 못했습니다.";
-    });
+    iframe.addEventListener('error', function(){ host.textContent = "지도를 불러오지 못했습니다."; });
 
     host.innerHTML = "";
     host.appendChild(iframe);
     host.setAttribute('data-map-rendered', '1');
   }
-
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', render);
-  } else {
-    render();
-  }
-  window.KingsMapRender = render; // debug hook
+  if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', render); else render();
+  window.KingsMapRender = render;
 })();
